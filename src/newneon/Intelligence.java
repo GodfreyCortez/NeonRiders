2c8ad989208747a3098aa52133c284558edc3dee
Godfrey Cortez <godfreycortez@outlook.com>
2018-07-28 22:59:08 -0400

    Added Intelligence.java

--- src/newneon/Intelligence.java
+++ src/newneon/Intelligence.java
@@ -2,18 +2,23 @@ package newneon;
 import java.util.*;
 
 import static newneon.Game_Loop.*;
+import static newneon.Game_Loop.PlayerDirections.*;
 
 
 //The purpose of this class is to act as the decision makers for the CPU players
 //in the game
 public class Intelligence
 {
-    PlayerDirections playerDirection = null;
+    PlayerDirections playerDirection = LEFT;
 
     public Intelligence(){}
 
     public PlayerDirections chooseDirection(PlayerDirections currentDirection)
     {
+        if(currentDirection == LEFT)
+        {
+
+        }
         return playerDirection;
     }
 }
